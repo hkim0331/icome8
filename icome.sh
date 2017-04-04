ee2dcfdbb536c13ef75ee51103164b4bd387e2e1
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2017-04-04 16:13:53 +0900

    forget to add icome.sh

---
+++ icome.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# wrapper is necessary for not to quit icome launched.
+
+ps ax | egrep '[i]come.rb' >/dev/null
+if [ "$?" -eq 0 ]; then
+    echo icome はすでに起動しています。
+    exit
+fi
+echo icome の起動には 5 秒くらいかかります。
+
+if [ -f /home/t/hkimura/icome8/icome.rb ]; then
+    ICOME=/home/t/hkimura/icome8/icome.rb
+    UCOME='druby://150.69.90.82:9007' nohup ${ICOME} 2>/dev/null &
+
+    # in isc, launch xwatch-ss simultaneously.
+    if [ -e /edu/bin/xwatch-ss ]; then
+        nohup /edu/bin/xwatch-ss 2>/dev/null &
+    fi
+else
+    echo "debug mode. consider to use --debug option."
+    ./icome.rb $@
+fi
+
