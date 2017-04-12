c2fc0171263ee9e79ae32da4a7c654d3b7c1ade1
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2017-04-12 16:41:18 +0900

    fix bug in launching xwatch-ss

--- icome.sh
+++ icome.sh
@@ -13,8 +13,11 @@ if [ -f /home/t/hkimura/icome8/icome.rb ]; then
     UCOME='druby://150.69.90.82:9007' nohup ${ICOME} 2>/dev/null &
 
     # in isc, launch xwatch-ss simultaneously.
-    if [ -e /edu/bin/xwatch-ss ]; then
-        nohup /edu/bin/xwatch-ss 2>/dev/null &
+    XWATCH=/home/t/hkimura/xwatch-ss
+    if [ -e ${XWATCH} ]; then
+        nohup ${XWATCH}/xwatch-ss.rb \
+              --image ${XWATCH}/images/ghost-busters.png \
+              --check /home/t/hkimura/Desktop/xwatch-ss.conf 2>/dev/null &
     fi
 else
     echo "debug mode. consider to use --debug option."
